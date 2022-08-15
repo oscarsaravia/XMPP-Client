d47afa54c77f52044457959f592c65161d876505
oscarsaravia <oscarsaraviard0013@gmail.com>
2022-08-15 14:05:36 -0600

    Added new funcionalities

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 import slixmpp
 from aioconsole import ainput
-
+from utils import menu_utils
 
 class Client(slixmpp.ClientXMPP):
     def __init__(self, jid, password):
@@ -13,20 +13,51 @@ class Client(slixmpp.ClientXMPP):
         self.register_plugin('xep_0199') # XMPP Ping
 
     async def start(self, event):
+        print("STARTING CLIENT...")
+        chat_menu = menu_utils.get_chat_interface()
         self.send_presence()
         await self.get_roster()
         online = True
         while online:
-            option_selected = int(input('Please, select an option ==> '))
+            print(chat_menu)
+            option_selected = int(input('==> '))
             if (option_selected == 1):
+                roster = self.client_roster.groups()
+                roster_values = roster.get("")
+                for x in range(len(roster_values)):
+                    print(x + 1, ': ', roster_values[x])
+            elif (option_selected == 2):
+                contact_to_add = input("Enter the contact email: ")
+                self.send_presence_subscription(pto= contact_to_add)
+                print('ADD CONTACT REQUEST SENT!')
+                
+            elif (option_selected == 3):
+                emisor = self.jid + ': '
                 receptor = 'oscarsaravia@alumchat.fun'
                 keep_chat = True
+                print('To exit back to menu, write exit()')
                 while keep_chat:
-                    message = await ainput('==> ')
-                    if message == 'exit':
+                    message = await ainput(emisor)
+                    if message == 'exit()':
                         keep_chat = False
                     self.send_private_message(receptor, message)
-                    print(message)
+            elif (option_selected == 4):
+                print()
+                contact_selected = await ainput('Contact JID: ')
+                roster = self.client_roster.groups()
+                contact_list = roster
+                # xmpp.contact_list(specific = contact_selected)
+            elif (option_selected == 5):
+                options = ['chat', 'away', 'xa Available', 'dnd']
+                menu_utils.show_presence_options()
+                option = await ainput('==> ')
+                message_test = 'HOLA MUNDO!'
+                if (int(option) >= 1 and int(option) <= 5):
+                    presence_value = options[int(option) - 1]
+                self.send_presence(pshow=presence_value, pstatus=message_test)
+            elif (option_selected == 6):
+                self.disconnect(wait=False)
+                online = False
 
     def message(self, msg):
         emisor = str(msg['from'])
@@ -36,11 +67,9 @@ class Client(slixmpp.ClientXMPP):
             # msg.reply("Thanks for sending\n%(body)s" % msg).send()
     
     def send_private_message(self, receptor, message = ''):
-		# print(f'Sending message to {to}')
-		# print(f'Message: {message}')
 	    self.send_message(
 			mto=receptor,
 			mbody=message,
 			mtype='chat'
 		)
-		# print('Message sent succefully')
+
--- utils/menu_utils.py
+++ utils/menu_utils.py
@@ -8,3 +8,26 @@ def get_menu_interface() -> str:
   '''
 
   return menu
+
+def get_chat_interface() -> str:
+  chat_menu = '''
+    === PLEASE, SELECT AN OPTION ===
+    1. Show contacts
+    2. Add new contact
+    3. Send message to user
+    4. Get contact info
+    5. Change Presence
+    6. Logout
+  '''
+
+  return chat_menu
+
+def show_presence_options() -> str:
+  presence_options = '''
+    === PLEASE SELECT AN OPTION ===
+    1. Available
+    2. Away
+    3. Not Available
+    4. Bussy
+  '''
+  print(presence_options)
